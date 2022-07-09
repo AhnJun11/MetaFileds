02cbd70488d1f59de7da8dc495331da3e331f0a3
anjo0 <anjo0@DESKTOP-N8DR02L>
2022-07-09 20:25:38 +0900

    프로필 사진 수정

--- src/main/resources/static/js/develop/login.js
+++ src/main/resources/static/js/develop/login.js
@@ -3,9 +3,10 @@
  */
 
 function login(){
+	location.href="/member/memberManagement";
 	if($("#user_id").val() == "admin"){
 		if($("#user_password").val() == "admin"){
-			location.href="/member/memberManagement";
+			
 		}	
 	}
 	;
