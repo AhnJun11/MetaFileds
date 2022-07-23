dd474e6bd3fc3e67bc99e1104a50d44c74141776
dowon <dowon@DESKTOP-OEDU5L5>
2022-07-24 01:40:22 +0900

    헤더 , 사이드바 ui수정 및 아이디 비밀번호 찾기 js 추가

--- src/main/java/metaFileds/co/kr/controller/URIController.java
+++ src/main/java/metaFileds/co/kr/controller/URIController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class URIController {
 	@GetMapping("/")
 	public String goIndex() {
-		return "index";
+		return "login";
 	}
 	
 	@GetMapping("loginPage/idPwdFind")
